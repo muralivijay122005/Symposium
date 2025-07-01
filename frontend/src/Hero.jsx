8412c14a7a8d4f73c7a26cfa6967e007f67439e9
guru1234cud <guru2006may@gmail.com>
2025-07-01 20:56:11 +0530

    trial

--- frontend/src/Hero.jsx
+++ frontend/src/Hero.jsx
@@ -3,28 +3,58 @@ import Background from "./assets/BG.jpg";
 import Countdown from "./Countdown";
 import Arrow from "./assets/Arrow-Rounded.svg";
 import "./Hero.css";
+import DecryptedText from "./components/DecryptedText";
 
 const Hero = () => {
   return (
-    <div className="relative bg-black w-full h-screen">
-      {/* Background Image */}
-      <img
-        src={Background}
-        alt="BG"
-        className="absolute opacity-30 top-0 left-0 w-full h-full object-cover z-0"
-      />
-      <p className="relative font-pexico z-20 text-cyan-600 text-center text-5xl top-50">
+    <div className="background">
+    <div className="relative w-full min-h-screen text-white flex flex-col items-center justify-start px-4 py-12 gap-8">
+
+      {/* Main Title */}
+      <p className="z-10 font-pexico text-cyan-500 text-center text-5xl">
         DEDSEC '25
       </p>
 
-      {/* Overlay + Timer */}
-      <div className="relative z-10 flex flex-col items-center justify-center h-full gap-4">
+      {/* Countdown + Button */}
+      <div className="z-10 flex flex-col items-center gap-4">
         <Countdown />
         <button className="pixel-btn relative flex items-center gap-2 font-pexico text-xl mt-4 px-6 py-3 pt-2 text-white ">
           Register Now
           <img src={Arrow} className="w-6 mt-1 rotate-270" />
         </button>
       </div>
+
+      {/* Animated Text */}
+      <div className="z-10 mt-10 text-center space-y-4">
+        <DecryptedText
+          text="This text animates when in view"
+          animateOn="view"
+          revealDirection="start"
+          speed={100}
+          sequential="true"
+        />
+        <DecryptedText
+          text="This text animates when in view"
+          animateOn="view"
+          revealDirection="start"
+          speed={100}
+          sequential="true"
+        />
+      </div>
+
+      {/* Map */}
+      <div className="z-10 mt-12 w-full flex justify-center">
+        <iframe
+          title="Velammal Engineering College"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4236.8364160551155!2d80.18907521102054!3d13.15001548659388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5264a10c856599%3A0xac3348f41097ba7f!2sVelammal%20Engineering%20College!5e0!3m2!1sen!2sin!4v1751381868317!5m2!1sen!2sin"
+          width="100%"
+          height="350"
+          className="rounded-xl w-full max-w-4xl border border-white/20 shadow-lg"
+          loading="lazy"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
     </div>
   );
 };
