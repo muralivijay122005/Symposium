043718ffe1f2c4c9b958c7cb10277c62eeef4976
guru1234cud <guru2006may@gmail.com>
2025-07-03 10:52:13 +0530

    dsadsa

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React,{useState} from "react";
+import Hero from "./Hero";
+import SplashScreen from "./SplashScreen";
+const App = () => {
+  const [showSplash, setShowSplash] = useState(true);
+
+  return (
+    <>
+      {showSplash ? (
+        <SplashScreen onComplete={() => setShowSplash(false)} />
+      ) : (
+        <Hero />
+        
+      )}
+      
+    </>
+  );
+};
+
+export default App;
---
+++ src/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Aurora from './components/Aurora';
+
+const Hero = () => {
+  return (
+    <>
+    <div className="bg-blue-950 h-200">
+      <Aurora
+      
+  colorStops={["#3A29FF", "#00FFFF"]}
+  blend={7}
+  amplitude={1}
+  speed={0.5}
+/>
+    </div>
+    <div className='font-modern-regular'>
+      Text
+    </div>
+    </>
+  )
+}
+
+export default Hero
