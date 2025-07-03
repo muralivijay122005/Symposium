ce32cdbc44f00adcdfc1b4e0b39bdeef340c544c
guru1234cud <guru2006may@gmail.com>
2025-07-03 15:50:16 +0530

    desc

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import Navbar from "./components/Navbar";
 import Hero from "./Hero";
 import About from "./pages/About";
+import EventCards from "./pages/EventCards";
+import Map from "./pages/Map";
+import ClickSpark from './components/ClickSpark';
 
 const App = () => {
 
   return (
     <>
     <div className="blue-bg min-h-screen">
-      <Hero />
-      <About/>
-      <Navbar/>
+      <ClickSpark
+        sparkColor='#fff'
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+      >
+        <Hero />
+        <About/>
+        <Navbar/>
+        <EventCards/>
+        <Map/>
+      </ClickSpark>
     </div>
     </>
   );
