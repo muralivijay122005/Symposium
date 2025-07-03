52d5b34c1f6d5c3b42b3b6a3e3d23670645e3c2f
guru1234cud <guru2006may@gmail.com>
2025-07-03 14:48:45 +0530

    dock

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -55,7 +55,7 @@ function DockItem({
       onFocus={() => isHovered.set(1)}
       onBlur={() => isHovered.set(0)}
       onClick={onClick}
-      className={`relative inline-flex items-center justify-center rounded-full bg-[#060010] border-neutral-700 border-2 shadow-md ${className}`}
+      className={`relative inline-flex items-center justify-center rounded-full border-white/20 backdrop-blur-md border-2 shadow-md ${className}`}
       tabIndex={0}
       role="button"
       aria-haspopup="true"
@@ -86,7 +86,7 @@ function DockLabel({ children, className = "", ...rest }) {
           animate={{ opacity: 1, y: -10 }}
           exit={{ opacity: 0, y: 0 }}
           transition={{ duration: 0.2 }}
-          className={`${className} absolute -top-6 left-1/2 w-fit whitespace-pre rounded-md border border-neutral-700 bg-[#060010] px-2 py-0.5 text-xs text-white`}
+          className={`${className} absolute -top-6 left-1/2 w-fit whitespace-pre rounded-md border border-white/20 backdrop-blur-md px-2 py-0.5 text-xs text-white`}
           role="tooltip"
           style={{ x: "-50%" }}
         >
@@ -139,7 +139,7 @@ export default function Dock({
           isHovered.set(0);
           mouseX.set(Infinity);
         }}
-        className={`${className} absolute bottom-2 left-1/2 transform -translate-x-1/2 flex items-end w-fit gap-4 rounded-full bg-blur-lg fixed border-neutral-700 border-2 pb-2 px-4`}
+        className={`${className} absolute bottom-2 left-1/2 transform -translate-x-1/2 flex items-end w-fit gap-4 rounded-full backdrop-blur-md fixed border-white/20 border-2 pb-2 px-4`}
         style={{
           height: panelHeight + 5 ,
         }}
@@ -157,7 +157,7 @@ export default function Dock({
             magnification={magnification}
             baseItemSize={baseItemSize}
           >
-            <DockIcon>{item.icon}</DockIcon>
+            <DockIcon className="backdrop-blur-md text-white">{item.icon}</DockIcon>
             <DockLabel>{item.label}</DockLabel>
           </DockItem>
         ))}
