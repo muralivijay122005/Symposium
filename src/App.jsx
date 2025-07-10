f5aea1c26ca90854ce6c9dde1465ad5e19138322
balakumaran1507 <145541052+balakumaran1507@users.noreply.github.com>
2025-07-10 14:56:23 +0530

    ddf

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import MainApp from "./MainApp";
 import CodeOfConduct from "./pages/CodeOfConduct";
 import CustomCursor from "./components/CustomCursor";
 import RegisterCards from "./pages/RegisterCards"; 
+import BusRoute from "./pages/BusRoutes";
 
 
 const App = () => {
   return (
     <Router>
       <CustomCursor />
-      
-        <Routes>
-          <Route path="/" element={<MainApp />} />
-          <Route path="/codeofconduct" element={<CodeOfConduct />} />
-          <Route path="/register" element={<RegisterCards />} />
-        </Routes>
-     
+
+      <Routes>
+        <Route path="/" element={<MainApp />} />
+        <Route path="/codeofconduct" element={<CodeOfConduct />} />
+        <Route path="/register" element={<RegisterCards />} />
+        <Route path="/busroutes" element={<BusRoute/>} />
+      </Routes>
     </Router>
   );
 };
